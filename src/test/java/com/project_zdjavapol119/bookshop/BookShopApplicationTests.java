76e07eb3a001f9422665fef4ab003493ea887db2
Pawel <pawel@pawel.com>
2023-01-14 13:59:20 +0100

    first commit

---
+++ src/test/java/com/project_zdjavapol119/bookshop/BookShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_zdjavapol119.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
